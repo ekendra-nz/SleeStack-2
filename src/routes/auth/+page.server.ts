428760904f1bcb7516867ea5fac00ade3ab38c30
ekendra-nz <le.pattillo.nz@gmail.com>
2024-07-31 19:47:01 +1200

    validate captcha

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-prototype-builtins */
 import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
+import { PRIVATE_TURNSTILE_SECRET_KEY } from '$env/static/private';
+import { errors } from '@playwright/test';
 
 export const actions: Actions = {
 	signup: async ({ request, locals: { supabase } }) => {
@@ -42,7 +44,16 @@ export const actions: Actions = {
 		const formData = await request.formData();
 		const email = formData.get('email') as string;
 		const password = formData.get('password') as string;
+		const token = formData.get('cf-turnstile-response');
+		const SECRET_KEY = PRIVATE_TURNSTILE_SECRET_KEY;
 
+		const { success, errors } = await validateToken(token, SECRET_KEY);
+
+		if (errors) {
+			return fail(400, { token, error: 'Failed CAPTCHA' });
+		} else {
+			console.log('CAPTCHA success', success);
+		}
 		const { error } = await supabase.auth.signInWithPassword({ email, password });
 		if (error) {
 			console.error(error);
@@ -52,3 +63,33 @@ export const actions: Actions = {
 		}
 	}
 };
+
+interface TokenValidateResponse {
+	'error-codes': string[];
+	success: boolean;
+	action: string;
+	cdata: string;
+}
+
+async function validateToken(token: string, secret: string) {
+	const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json'
+		},
+		body: JSON.stringify({
+			response: token,
+			secret: secret
+		})
+	});
+
+	const data: TokenValidateResponse = await response.json();
+
+	return {
+		// Return the status
+		success: data.success,
+
+		// Return the first error if it exists
+		errors: data['error-codes']?.length ? data['error-codes'][0] : null
+	};
+}
